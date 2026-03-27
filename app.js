0e1bb7828371a70c1834e4160a11eed44cecdb93
Thomas MENAGE <thomas.menage@hei.fr>
2026-03-27 12:15:37 +0100

    code cleaning

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var studentsRouter = require("./routes/students");
 var lessonsRouter = require("./routes/lessons");
 var invoicesRouter = require("./routes/invoices");
 var ressourcesRouter = require("./routes/ressources");
-// var messagesRouter = require("./routes/messages");
 var messagesBisRouter = require("./routes/messagesBis");
 
 var app = express();
@@ -65,7 +64,6 @@ app.use("/lessons", lessonsRouter);
 app.use("/invitations", invitationsRouter);
 app.use("/invoices", invoicesRouter);
 app.use("/ressources", ressourcesRouter);
-// app.use("/messages", messagesRouter);
 app.use("/messagesBis", messagesBisRouter);
 
 module.exports = app;
